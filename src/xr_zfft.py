c71f15711f289de5f88bfe4d1a8e51bbf7221a89
Michael Eaton <mpeaton@amdgtechnologies.com>
2019-03-19 08:14:57 -0400

    "Post Trying Source Disorder"

--- src/xr_zfft.py
+++ src/xr_zfft.py
@@ -1,15 +1,19 @@
 import subprocess
-from distutils.core import setup, Extension
+from numpy.distutils.core import setup, Extension
 from distutils.sysconfig import get_python_lib,get_python_inc
-import os
+from os.path import join
+from os import environ
+from numpy.distutils.misc_util import Configuration
+from glob import glob
 
-prefix = os.environ.get('CONDA_PREFIX','/usr/local')
+prefix = environ.get('CONDA_PREFIX','/usr/local')
 site_packages = get_python_lib()
 
+srcdir = 'src'
 fn = 'zfft.h'
 name = fn.split('.')[0]
 
-libs = ['ndtypes','gumath', 'xnd','xndtools/kernel_generator','numpy/f2py/src','numpy/core/include']
+libs = ['fftpack','ndtypes','gumath', 'xnd','xndtools/kernel_generator','numpy/f2py/src','numpy/core/include']
 lib_dirs = [f'{site_packages}/{lib}' for lib in libs]
 system_libs = []
 
@@ -40,8 +44,26 @@ if build_archive == 'y':
        
        print(c_string)
        subprocess.call(c_string,shell=True)
+       
+       subprocess.call(['gfortran','-c','-fPIC','-g',*glob(join('fftpack','*.f'))])
+       subprocess.call(['ar','rcs',f'lib{name}-kernels.a',f'{name}-kernels.o',f'{name}.o',*glob(join('fftpack','*.o'))])
+       subprocess.call(['mv',f'lib{name}-kernels.a',join(f'{prefix}','lib')])
+# fftpack objects
+# config = Configuration('fftpack','',None)
 
-       subprocess.call(['ar','rcs',f'lib{name}-kernels.a',f'{name}-kernels.o',f'{name}.o'])
+# fftpack_src = [join('src/fftpack','*.f')]
+# config.add_library('fftpack', sources=fftpack_src)
+
+# dfftpack_src = [join('src/dfftpack','*.f')]
+# config.add_library('dfftpack', sources=dfftpack_src)
+
+# config.add_extension('zfft',
+#         sources='src/zfft.c',
+#         libraries=['dfftpack', 'fftpack'],
+#         include_dirs=['src'],
+#         depends=(dfftpack_src + fftpack_src),
+#         extra_f77_compile_args=['-fPIC'])
+
 
 install_module=input("Install module y/[n]").lower()
 if install_module=='y':
@@ -49,11 +71,16 @@ if install_module=='y':
                      include_dirs = lib_dirs,
                      libraries = ['zfft-kernels'] + system_libs + libs,
                      library_dirs = ['.'] + lib_dirs,
-                     sources = ['zfft.c']
+                     sources = [f'{name}.c']
                      )
 
-       setup (name = 'zfft',
+       setup (#name = 'zfft',
               version = '0.1',
               description = 'This is a gumath kernel extension that performs fftpack operations using XND containers',
-              ext_modules = [module1]
+              ext_modules = [module1],
+       #       ÷ **config.todict()
               )
+
+
+
+
